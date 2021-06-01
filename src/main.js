90f3f873af75bef845951637ab19a406ce55dcfc
xiaokinglong <xiaokinglong@gmail.com>
2021-06-02 00:02:23 +0800

    ant-design-vue 主题动态切换案列

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+Vue.use(Antd);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
